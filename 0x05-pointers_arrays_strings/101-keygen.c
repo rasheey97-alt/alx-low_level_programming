e2485708b2ee595c156203a0eb7e505fbb4397ef
rasheey97_alt <rasheey97@yahoo.com>
2022-04-21 21:54:54 -0700

    task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,20 @@
-#include "main.h"
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
+ * main - original function
+ * @n: new varirable
+ *
+ * return 0.
  */
 int main(void)
 {
-int num;
+int n;
 
 srand(time(0));
-num = rand();
-printf("%i\n", num);
+n = rand();
+	printf("Tada! Congrats %c\n", n);
+
 return (0);
 }
