8149cc0f04447742371f179038b7112c75cbde05
rasheey97_alt <rasheey97@yahoo.com>
2022-06-03 16:25:40 -0700

    task 2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,13 +10,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t file, fread, fwrite;
 	char *totalSize;
-	
+
 	totalSize = malloc(sizeof(char) * letters);
 	if (totalSize == NULL)
 		return (0);
 	if (filename == NULL)
 		return (0);
-	
+
 	file = open(filename, O_RDONLY);
 	if (file == -1)
 		return (0);
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fread == -1)
 		return (0);
 	fwrite = write(STDOUT_FILENO, totalSize, fread);
-    
+
 	if (fwrite == -1)
 		return (0);
 	close(file);
