6784a9d82ba190c4c907953d2d3a09bec6526bdb
rasheey97_alt <rasheey97@yahoo.com>
2022-06-03 17:08:37 -0700

    task 01

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,29 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
+ * create_file - creates a file
+ * @filename: the name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content)
 {
-	ssize_t file, fread, fwrite;
-	char *totalSize;
+	int file, fwrite, i;
 
-	totalSize = malloc(sizeof(char) * letters);
-	if (totalSize == NULL)
-		return (0);
 	if (filename == NULL)
-		return (0);
-	file = open(filename, O_RDONLY);
+		return (-1);
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (file == -1)
-		return (0);
-	fread = read(file, totalSize, letters);
-	if (fread == -1)
-		return (0);
-	fwrite = write(STDOUT_FILENO, totalSize, fread);
-	if (fwrite == -1)
-		return (0);
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i]; i++)
+			;
+		fwrite = write(file, text_content, i);
+		if (fwrite == -1)
+			return (-1);
+	}
 	close(file);
-	free(totalSize);
-	return (fwrite);
+	return (1);
 }
