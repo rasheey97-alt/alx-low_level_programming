22ff7f26bbd9a91b304ef643a64e8e3258cd62bc
rasheey97_alt <rasheey97@yahoo.com>
2022-06-03 17:41:06 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,13 +9,13 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int file, fwrite, i;
-	
+
 	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_APPEND | O_RDWR, 0664);
 	if (file == -1)
 		return (-1);
-			
+
 	if (text_content != NULL)
 	{
 	for (i = 0; text_content[i]; i++)
